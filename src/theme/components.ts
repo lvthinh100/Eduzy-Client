35bf90c37f33f7609ffe7b61761e59598eae9c14
THÔNG TRẦN VĂN <21120565@student.hcmus.edu.vn>
2023-10-01 16:29:09 +0700

    Fix Mobile UI

--- src/theme/components.ts
+++ src/theme/components.ts
@@ -1,17 +1,18 @@
-import { ButtonProps } from "@mui/material";
-import { Theme } from "@mui/material";
+import { ButtonProps } from '@mui/material';
+import { Theme } from '@mui/material';
 
-import Digital from "../assets/Fonts/digital.ttf";
-import ArialRounded from "../assets/Fonts/ARLRDBD.ttf";
-import SegoeUISemiBold from "../assets/Fonts/SegoeUI-SemiBold.ttf";
-import ArialUnicodeMS from "../assets/Fonts/ArialUnicodeMS.ttf";
-import _SegoeUIBold from "../assets/Fonts/Segoe-UI-Bold.ttf";
-import _SegoeUINormal from "../assets/Fonts/SVN-Segoe-UI.ttf";
-import HandWriting from "../assets/Fonts/Merienda-VariableFont_wght.ttf";
-import Signature from "../assets/Fonts/BrothersideSignature-w13o6.otf";
-import OswaldSemiBold from "../assets/Fonts/Oswald-SemiBold.ttf";
+import Digital from '../assets/Fonts/digital.ttf';
+import ArialRounded from '../assets/Fonts/ARLRDBD.ttf';
+import SegoeUISemiBold from '../assets/Fonts/SegoeUI-SemiBold.ttf';
+import ArialUnicodeMS from '../assets/Fonts/ArialUnicodeMS.ttf';
+import _SegoeUIBold from '../assets/Fonts/Segoe-UI-Bold.ttf';
+import _SegoeUINormal from '../assets/Fonts/SVN-Segoe-UI.ttf';
+import HandWriting from '../assets/Fonts/Merienda-VariableFont_wght.ttf';
+import Signature from '../assets/Fonts/BrothersideSignature-w13o6.otf';
+import OswaldSemiBold from '../assets/Fonts/Oswald-SemiBold.ttf';
+import _MontserratBold from '../assets/Fonts/Montserrat-Bold.ttf';
 
-declare module "@mui/material/Button" {
+declare module '@mui/material/Button' {
   interface ButtonPropsVariantOverrides {
     gradient: true;
     gradient2: true;
@@ -23,48 +24,48 @@ const components = {
   MuiButton: {
     variants: [
       {
-        props: { variant: "gradient" } as ButtonProps,
+        props: { variant: 'gradient' } as ButtonProps,
         style: {
           backgroundImage: `linear-gradient(to right, #FCDC93, #f08ad3 75%)`,
-          "&:hover": {
-            opacity: "0.7",
+          '&:hover': {
+            opacity: '0.7',
           },
-          "&:disabled": {
+          '&:disabled': {
             backgroundImage: `linear-gradient(to right, white, gray 75%)`,
-            color: "white",
+            color: 'white',
           },
-          color: "white",
-          borderRadius: "20px",
+          color: 'white',
+          borderRadius: '20px',
         },
       },
       {
-        props: { variant: "gradient2" } as ButtonProps,
+        props: { variant: 'gradient2' } as ButtonProps,
         style: {
           backgroundImage: `linear-gradient(to right, #9fecf8, #f2a9df 75%)`,
-          "&:hover": {
-            opacity: "0.7",
+          '&:hover': {
+            opacity: '0.7',
           },
-          "&:disabled": {
+          '&:disabled': {
             backgroundImage: `linear-gradient(to right, white, gray 75%)`,
-            color: "white",
+            color: 'white',
           },
-          color: "white",
-          borderRadius: "20px",
+          color: 'white',
+          borderRadius: '20px',
         },
       },
       {
-        props: { variant: "gradient3" } as ButtonProps,
+        props: { variant: 'gradient3' } as ButtonProps,
         style: {
           backgroundImage: `linear-gradient(to right, #FCDC93, #f08ad3 100%)`,
-          "&:hover": {
-            opacity: "0.7",
+          '&:hover': {
+            opacity: '0.7',
           },
-          "&:disabled": {
+          '&:disabled': {
             backgroundImage: `linear-gradient(to right, white, gray 75%)`,
-            color: "white",
+            color: 'white',
           },
-          color: "white",
-          borderRadius: "20px",
+          color: 'white',
+          borderRadius: '20px',
         },
       },
     ],
@@ -72,20 +73,20 @@ const components = {
   MuiDateCalendar: {
     styleOverrides: {
       root: ({ theme }: { theme: Theme }) => ({
-        "& .MuiTypography-root": {
-          fontWeight: "inherit",
-          color: "inherit",
+        '& .MuiTypography-root': {
+          fontWeight: 'inherit',
+          color: 'inherit',
         },
-        "& .MuiDayCalendar-header": {
-          fontWeight: "bolder",
-          color: "#bea1a6",
+        '& .MuiDayCalendar-header': {
+          fontWeight: 'bolder',
+          color: '#bea1a6',
         },
-        "& .MuiPickersDay-dayOutsideMonth": {
-          fontWeight: "bolder",
-          color: "#bea1a6",
+        '& .MuiPickersDay-dayOutsideMonth': {
+          fontWeight: 'bolder',
+          color: '#bea1a6',
           opacity: 0.8,
         },
-        "& .MuiPickersDay-today": {
+        '& .MuiPickersDay-today': {
           backgroundColor: theme.palette.highlighter.main,
         },
       }),
@@ -127,6 +128,13 @@ const components = {
         font-display: swap;
         font-weight: 800;
         src: url(${_SegoeUIBold}) format('truetype');
+      }
+      @font-face {
+        font-family: '_MontserratBold';
+        font-style: normal;
+        font-display: swap;
+        font-weight: 800;
+        src: url(${_MontserratBold}) format('truetype');
       }
       @font-face {
         font-family: '_SegoeUINormal';
