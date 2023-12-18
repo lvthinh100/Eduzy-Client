0c71604c24ae2df106a1624db66a423741595f35
THÔNG TRẦN VĂN <21120565@student.hcmus.edu.vn>
2023-12-18 16:14:16 +0700

    fix clock

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -14,6 +14,10 @@ const Countdown: React.FC<PropsType> = ({ date, onTimeout }) => {
   const [value, setValue] = React.useState('00:00:00');
   const { timediff } = useAppSelector((state) => state.app);
   const startUpdatingCountdown = useCallback(() => {
+    if (!dayjs.isDayjs(date)) {
+      console.error('Invalid date prop. Please provide a valid dayjs object.');
+      return;
+    }
     const timer = setInterval(() => {
       const now = dayjs().add(timediff ? timediff : 0, 'second');
       const milisecond = date.diff(now) + 1000;
